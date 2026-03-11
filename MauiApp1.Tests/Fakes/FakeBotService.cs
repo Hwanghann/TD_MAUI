c50ad77a290558e99083bfc476f75fd6a1fc09e8
DUCHATEAU Camille <camille.duchateau@ccicampus.fr>
2026-03-11 15:59:15 +0100

    TD4/Tests: ajout d'un projet de tests

---
+++ MauiApp1.Tests/Fakes/FakeBotService.cs
@@ -0,0 +1,18 @@
+﻿using MauiApp1.Services;
+
+namespace MauiApp1.Tests.Fakes;
+
+public class FakeBotService : IBotService
+{
+    private readonly int moveToPlay;
+
+    public FakeBotService(int moveToPlay)
+    {
+        this.moveToPlay = moveToPlay;
+    }
+
+    public int ChooseMove(string[] board, string botSymbol, string humanSymbol)
+    {
+        return moveToPlay;
+    }
+}
